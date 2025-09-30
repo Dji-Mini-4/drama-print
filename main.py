75159a4942e0a506e7c00ff3947a235a05abb66e
J4YD3N <jaydenliu2032@gmail.com>
2025-09-29 21:11:29 -0700

    MAIN

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+import time
+
+class Dramaprint:
+    """The class for dramatic printing"""
+  
+    def __init__(self, delay=0.05): # default delay is 0.05
+        self.delay = delay
+
+    def setDelay(self, delay):
+        # sets self.delay to argument `delay`
+        self.delay = delay
+
+    def print(self, *args, sep=' ', end='\n'):
+        """
+        Dramatically prints text to the console, like print() but slower & cooler.
+        
+        Parameters:
+            args  — multiple strings or values to print
+            sep   — separator between arguments (default: space)
+            end   — what to print at the end (default: newline)
+        
+        Also the delay is set on creating object or by setDelay
+        """
+
+        output = sep.join(str(arg) for arg in args) + end
+        for char in output:
+            sys.stdout.write(char)
+            sys.stdout.flush() # flush immediately to terminal
+            time.sleep(self.delay) # apply delay
+
+# Example use:
+# Umcomment for testing:
+# printObj = Dramaprint()
+# print = printObj.print
+# print('Hello World!')
+# printObj.setDelay(0.1) 
+# print('Hello Again!')    Now the output should be slower
